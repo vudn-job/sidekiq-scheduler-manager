8be7d13fab7e1872423f1129cd3db2940e1b976d
daonguyenvu <vu.dao@innogram.co>
2017-05-04 11:20:32 +0700

    refactor

--- lib/sidekiq-scheduler-manager.rb
+++ lib/sidekiq-scheduler-manager.rb
@@ -1,7 +1,10 @@
 require "sidekiq-scheduler-manager/version"
 
-require 'sidekiq'
-require 'tilt/erb'
+# require 'sidekiq'
+# require 'sidekiq-scheduler'
+# require 'tilt/erb'
+
+require 'sidekiq-scheduler/schedule'
 
 module SidekiqSchedulerManager
   # Your code goes here...
